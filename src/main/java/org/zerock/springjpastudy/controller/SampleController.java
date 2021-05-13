bd00bc081e64101297c5c9e1ad8cef6d9e6edd24
prodo-developer <wjdals300@gmail.com>
2021-05-13 14:05:20 +0900

    FIX : 타임리프 실습에 필요한 객체 생성

--- src/main/java/org/zerock/springjpastudy/controller/SampleController.java
+++ src/main/java/org/zerock/springjpastudy/controller/SampleController.java
@@ -2,8 +2,15 @@ package org.zerock.springjpastudy.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.springjpastudy.entity.dto.SampleDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/sample")
@@ -15,4 +22,18 @@ public class SampleController {
         log.info("ex..........");
     }
 
+    @GetMapping("/ex2")
+    public void exModel(Model model) {
+        List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(i -> {
+            SampleDTO dto = SampleDTO.builder()
+                            .sno(i)
+                            .first("First.." + i)
+                            .last("Last.." + i)
+                            .regTime(LocalDateTime.now())
+                            .build();
+            return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("list", list);
+    }
 }
